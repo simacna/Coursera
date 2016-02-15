8518972165c1e61bc04c5cbe3edceb930876ecfb
sina sima <simacna@gmail.com>
2016-02-15 10:10:39 -0500

    adding relations between tables

--- RailsActiveRecords/advanced_ar/db/migrate/20160212212407_create_people.rb
+++ RailsActiveRecords/advanced_ar/db/migrate/20160212212407_create_people.rb
@@ -9,3 +9,4 @@ class CreatePeople < ActiveRecord::Migration
     end
   end
 end
+
--- RailsActiveRecords/advanced_ar/db/seeds.rb
+++ RailsActiveRecords/advanced_ar/db/seeds.rb
@@ -1,5 +1,5 @@
-Job.destroy_all
-Person.destroy_all
+# Job.destroy_all
+# Person.destroy_all
 
 Person.create! [
   { first_name: "Kalman", last_name: "Smith", age: 33, login: "kman", pass: "abc123" },
@@ -11,20 +11,30 @@ Person.create! [
   { first_name: "LeBron", last_name: "James", age: 30, login: "bron", pass: "need more rings" }
 ]
 
-Person.find_by!(first_name: "Kalman").create_personal_info(height: 6.0, weight: 210)
-Person.find_by!(first_name: "John", last_name: "Whatever").create_personal_info(height: 5.3, weight: 175)
-Person.find_by!(first_name: "Michael").create_personal_info(height: 5.5, weight: 200)
-Person.find_by!(first_name: "Josh").create_personal_info(height: 6.5, weight: 235)
-Person.find_by!(first_name: "John", last_name: "Smith").create_personal_info(height: 6.0, weight: 210)
-Person.find_by!(first_name: "Bill").create_personal_info(height: 6.1, weight: 199)
-Person.find_by!(first_name: "LeBron").create_personal_info(height: 7.0, weight: 250)
+# Person.find_by!(first_name: "Kalman").create_personal_info(height: 6.0, weight: 210)
+# Person.find_by!(first_name: "John", last_name: "Whatever").create_personal_info(height: 5.3, weight: 175)
+# Person.find_by!(first_name: "Michael").create_personal_info(height: 5.5, weight: 200)
+# Person.find_by!(first_name: "Josh").create_personal_info(height: 6.5, weight: 235)
+# Person.find_by!(first_name: "John", last_name: "Smith").create_personal_info(height: 6.0, weight: 210)
+# Person.find_by!(first_name: "Bill").create_personal_info(height: 6.1, weight: 199)
+# Person.find_by!(first_name: "LeBron").create_personal_info(height: 7.0, weight: 250)
 
-Person.first.jobs.create! [
-  { title: "Developer", company: "MS", position_id: "#1234" },
-  { title: "Developer", company: "MS", position_id: "#1235" }
-]
+# Person.first.jobs.create! [
+#   { title: "Developer", company: "MS", position_id: "#1234" },
+#   { title: "Developer", company: "MS", position_id: "#1235" }
+# ]
 
-Person.last.jobs.create! [
-  { title: "Sr. Developer", company: "MS", position_id: "#5234" },
-  { title: "Sr. Developer", company: "MS", position_id: "#5235" }
-]
+# Person.last.jobs.create! [
+#   { title: "Sr. Developer", company: "MS", position_id: "#5234" },
+#   { title: "Sr. Developer", company: "MS", position_id: "#5235" }
+# ]
+
+# Person.create! [
+#   { first_name: "Kalman", last_name: "Smith", age: 33},
+#   { first_name: "John", last_name: "Whatever", age: 27},
+#   { first_name: "Michael", last_name: "Smith", age: 15},
+#   { first_name: "Josh", last_name: "Oreck", age: 57},
+#   { first_name: "John", last_name: "Smith", age: 27},
+#   { first_name: "Bill", last_name: "Gates", age: 75},
+#   { first_name: "LeBron", last_name: "James", age: 30}
+# ]
