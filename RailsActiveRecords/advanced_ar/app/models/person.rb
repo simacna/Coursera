a6068182b88620db677fff7684536765c6253570
sina sima <simacna@gmail.com>
2016-02-16 10:41:08 -0500

    finishing the activerecords relationships

--- RailsActiveRecords/advanced_ar/app/models/person.rb
+++ RailsActiveRecords/advanced_ar/app/models/person.rb
@@ -1,10 +1,14 @@
 class Person < ActiveRecord::Base
   has_one :personal_info, dependent: :destroy
-  has_one :salary_range
+
   has_many :jobs
+  has_many :approx_salaries, through: :jobs, source: :salary_range
   has_and_belongs_to_many :hobbies
   #class_name: 'Modelname'
   #want to be
 
-
+  def max_salary
+    approx_salaries.maximum(:max_salary)
+  end
+  
 end
