a9a1740b5a2841d80c5f1afcafaafc4881cca35c
sina sima <simacna@gmail.com>
2016-03-01 10:38:00 -0500

    nested resources

--- rails/ClassCode/i_reviewed/config/routes.rb
+++ rails/ClassCode/i_reviewed/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do
+    #notes is a nested resource
+    #below will only allow 2/7 resources
+    resources :notes, only: [:create, :destroy]
+  end
   root to: 'books#index'
 end
