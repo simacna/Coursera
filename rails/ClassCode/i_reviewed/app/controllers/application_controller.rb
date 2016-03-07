2dede9a684410b2cea7d6e7af1403c58631f4cc9
sina sima <simacna@gmail.com>
2016-03-07 15:11:06 -0500

    finished authentication

--- rails/ClassCode/i_reviewed/app/controllers/application_controller.rb
+++ rails/ClassCode/i_reviewed/app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #before you do anything else, make sure you're logged in:
+  before_action :ensure_login
+
+  # protected scope - other controllers are allowed to 
+  # inherit vs private scope where they can't
+  protected
+    def ensure_login
+      #always go to login page unless session contains
+      #reviewer_id
+      redirect_to login_path unless session[:reviewer_id]
+    end
 end
--- rails/ClassCode/i_reviewed/app/controllers/sessions_controller.rb
+++ rails/ClassCode/i_reviewed/app/controllers/sessions_controller.rb
@@ -1,12 +1,26 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_login, only: [:new, :create]
   
   def new
+    #Login page - new.html.erb
   end
 
   def create
-  end
+    #binding.pry
+    reviewer = Reviewer.find_by(name: params[:reviewer][:name])
+    password = params[:reviewer][:password]
+
+    if reviewer && reviewer.authenticate(password)
+      session[:reviewer_id] = reviewer.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      redirect_to login_path, alert: "Invalid login credentials"
+    end
+end
 
   def destroy
+    reset_session #a helper rails provides, wipe out session and everything in it
+    redirect_to login_path, notice: "You have been logged out"
   end
 
 end
