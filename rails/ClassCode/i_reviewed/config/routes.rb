bc54ec4dc581cf9737bf6e7e8a42a12779a97ce4
sina sima <simacna@gmail.com>
2016-03-07 14:20:26 -0500

    creating session login ability

--- rails/ClassCode/i_reviewed/config/routes.rb
+++ rails/ClassCode/i_reviewed/config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  # below three were created when sessions controller was created
+
+  # get 'sessions/new'
+
+  # get 'sessions/create'
+
+  # get 'sessions/destroy'
+  root to: 'books#index'
+
   resources :books do
     #notes is a nested resource
     #below will only allow 2/7 resources
     resources :notes, only: [:create, :destroy]
   end
-  root to: 'books#index'
+  resources :sessions, only: [:new, :create, :destroy]
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
 end
