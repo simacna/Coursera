a5c19c622df1d2f6e46748a0b3e1d35e992a10d9
sina sima <simacna@gmail.com>
2016-03-09 12:32:03 -0500

    adding helper methods for logged in sessions

--- rails/ClassCode/i_reviewed/app/controllers/application_controller.rb
+++ rails/ClassCode/i_reviewed/app/controllers/application_controller.rb
@@ -5,13 +5,23 @@ class ApplicationController < ActionController::Base
 
   #before you do anything else, make sure you're logged in:
   before_action :ensure_login
+  helper_method :logged_in?, :current_user
 
   # protected scope - other controllers are allowed to 
   # inherit vs private scope where they can't
   protected
+
     def ensure_login
       #always go to login page unless session contains
       #reviewer_id
       redirect_to login_path unless session[:reviewer_id]
+    end
+
+    def logged_in?
+      session[:reviewer_id] #nil is false
+    end
+
+    def current_user
+      @current_user ||= Reviewer.find(session[:reviewer_id])
     end
 end
