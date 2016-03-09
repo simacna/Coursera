b0de6533eed108d92f2ab7be32ee27aeb1a69ae1
sina sima <simacna@gmail.com>
2016-03-09 12:53:23 -0500

    added authorization to the books

--- rails/ClassCode/i_reviewed/app/controllers/books_controller.rb
+++ rails/ClassCode/i_reviewed/app/controllers/books_controller.rb
@@ -4,7 +4,11 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    #below would give us all books where as we want only books allowed to be seen
+    #by a current user
+    #@books = Book.all
+
+    @books = current_user.books.all
   end
 
   # GET /books/1
@@ -14,7 +18,8 @@ class BooksController < ApplicationController
 
   # GET /books/new
   def new
-    @book = Book.new
+    #@book = Book.new
+    @book = current_user.books.new
   end
 
   # GET /books/1/edit
@@ -24,7 +29,9 @@ class BooksController < ApplicationController
   # POST /books
   # POST /books.json
   def create
-    @book = Book.new(book_params)
+    #@book = Book.new(book_params)
+    #below automatically assigns the book to the user
+    @book = current_user.books.new(book_params)
 
     respond_to do |format|
       if @book.save
@@ -64,7 +71,8 @@ class BooksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
-      @book = Book.find(params[:id])
+     # @book = Book.find(params[:id])
+     @book = current_user.books.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
