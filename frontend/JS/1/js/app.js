f5bb5a63e1c7b2f3df0aeb73427b6a77a641dee0
sina sima <simacna@gmail.com>
2016-04-06 17:58:48 -0400

    different event handlers

--- frontend/JS/1/js/app.js
+++ frontend/JS/1/js/app.js
@@ -4,48 +4,56 @@
 document.addEventListener("DOMContentLoaded", 
   function(event) {
 
-  function sayIt(event){
-    //this should be printing MouseEvent {isTrusted: true}
-    console.log(event)
-    // console.log(this);
-     var name = document.getElementById("name").value;
-     var message = "<h2> Hello " + name + "</h2>";
-     //console.log(message);
+    function sayIt(t){
+      //this should be printing MouseEvent {isTrusted: true}
+      console.log(t)
+      // console.log(this);
+       var name = document.getElementById("name").value;
+       var message = "<h2> Hello " + name + "</h2>";
+       //console.log(message);
 
-    document.getElementById("content").innerHTML =
-    message;
+      document.getElementById("content").innerHTML =
+      message;
 
-    //var button = document.querySelector("button").textContent;
-    //console.log(button);
+      //var button = document.querySelector("button").textContent;
+      //console.log(button);
 
-    this.textContent = "Said it!";
-    //document.querySelector("button").textContent= name;
+      this.textContent = "Said it!";
+      //document.querySelector("button").textContent= name;
 
-    if (name === "student"){
-      // console.log("student");
-      // var title = document.getElementById("title");
-      // console.log(title);
-      // title.innerHTML(title + "welcome!");
+      if (name === "student"){
+        // console.log("student");
+        // var title = document.getElementById("title");
+        // console.log(title);
+        // title.innerHTML(title + "welcome!");
 
 
-      //below we could also use .querySelector("h1")
-      //since there's only one h1 on the page
-      
-      var title = document
-      .querySelector("#title")
-      .textContent;
+        //below we could also use .querySelector("h1")
+        //since there's only one h1 on the page
+        
+        var title = document
+        .querySelector("#title")
+        .textContent;
 
-      title += " loveeee";
+        title += " loveeee";
 
-      document.querySelector("#title").textContent = title;
-        }
+        document.querySelector("#title").textContent = title;
+      }
     }
-  });
+
+      document.querySelector("button").addEventListener("click", sayIt);
+      document.querySelector("body").addEventListener("mousemove", function(event){
+        if(event.shiftKey === true){
+          console.log(event.clientX);
+          console.log(event.clientY);
+        }
+      
+      });
+    });
 
 // Below are different ways of handling events
 //without adding onclick to html
 
-document.querySelector("button")
-.addEventListener("click", sayIt);
 
 // document.querySelector("button").onclick = sayIt;
+
