df8881a463e7cad42ed518fc1a75af906c9a1b42
sina sima <simacna@gmail.com>
2016-04-12 16:31:34 -0400

    adding functionality

--- frontend/JS/HTTP/lecture57/js/ajax-utils.js
+++ frontend/JS/HTTP/lecture57/js/ajax-utils.js
@@ -1,1 +1,21 @@
-ajax-utils.js
+(function(global){
+
+  //set up a namespace for our utility
+  var ajaxUtils = {};
+
+  //Returns an HTTP request object
+
+  function getRequestObject(){
+    if (window.XMLHttpRequest()){
+      return (new XMLHttpRequest());
+    }
+    else if(window.ActiveXObject){
+      //for very old IE browsers (optional)
+      return (new ActiveXObject("Microsoft.XMLHTTP"));
+    }
+    else{
+      global.alert("Ajax is not supported");
+      return(null);
+    }
+  }
+})
