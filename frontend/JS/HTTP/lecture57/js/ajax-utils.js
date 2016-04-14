6ef175a9de3e2031d6d9d63af0926c82c133edcf
sina sima <simacna@gmail.com>
2016-04-14 00:34:37 -0400

    closur ajax

--- frontend/JS/HTTP/lecture57/js/ajax-utils.js
+++ frontend/JS/HTTP/lecture57/js/ajax-utils.js
@@ -1,6 +1,6 @@
 (function(global){
-
   //set up a namespace for our utility
+  //we attach things to ajaxUtils that we want to expose to the outside world
   var ajaxUtils = {};
 
   //Returns an HTTP request object
@@ -18,4 +18,34 @@
       return(null);
     }
   }
-})
+
+  //Makes an ajax GET request to 'requestUrl'
+
+
+
+  ajaxUtils.sendGetRequest = 
+    function(requestUrl, responseHandler){
+      var request = getRequestObject();
+      request.onreadystatechange = function(){
+     
+        handleResponse(request, responseHandler);
+      };
+      request.open("GET", requestUrl, true);
+      request.send(null); //for post only
+    }
+
+  //only calls user provided 'responseHandler'
+  //function if response is ready
+  //and not an error
+
+  function handleResponse(request, responseHandler){
+    if ((request.readystate == 4) &&
+        (request.status == 200)){
+          responseHandler(request);
+    }
+  }
+
+  // expose utility to the global object
+  global.$ajaxUtils = ajaxUtils;
+
+})(window);
--- rails/HW/HW2/todolists/app/models/user.rb
+++ rails/HW/HW2/todolists/app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
+  
   has_one :profile, dependent: :destroy
   has_many :todo_lists
 
   has_many :todo_items, through: :todo_lists, source: :todo_items
   validates :username, presence: true
+
 end
